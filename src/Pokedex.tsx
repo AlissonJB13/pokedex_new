95c79956385e63636582eb955730adc2127bdd56
AlissonJB13 <alissonjb13@gmail.com>
2025-07-30 20:41:05 -0300

    feat: enhance Pokedex component with loading message and pagination controls

--- src/Pokedex.tsx
+++ src/Pokedex.tsx
@@ -1,4 +1,4 @@
-import { useState, type ChangeEvent, type FormEvent } from "react";
+import { useEffect, useState, type ChangeEvent, type FormEvent } from "react";
 import pokedexImage from "./assets/pokedex.png";
 import {
   PokedexContainer,
@@ -13,6 +13,8 @@ const Pokedex = () => {
   const [data, setData] = useState<IPokemon>();
   const [pokemonInput, setPokemonInput] = useState(""); // controle do input
   const [pokemonImage, setPokemonImage] = useState<string>();
+  const [message, setMessage] = useState<string>();
+  const [pokemonSearch, setPokemonSearch] = useState(1);
 
   const fetchPokemon = async (pokemon: string) => {
     const apiResponse = await fetch(
@@ -25,26 +27,47 @@ const Pokedex = () => {
     return data;
   };
 
-  const renderPokemon = async (pokemon: string) => {
+  const renderPokemon = async (pokemon: any) => {
+    setMessage("Loading...");
     try {
       const data = await fetchPokemon(pokemon);
+
       setData(data);
       setPokemonImage(
         data.sprites.versions["generation-v"]["black-white"].animated
           .front_default
       );
+      setPokemonSearch(data.id);
+      setPokemonInput("");
     } catch (error) {
       console.error(error);
       setData(undefined);
+      setMessage("Not Found :C");
       setPokemonImage("");
     }
   };
+
+  useEffect(() => {
+    renderPokemon(pokemonSearch);
+  }, [pokemonSearch]);
 
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const value = pokemonInput.trim().toLowerCase();
     if (!value) return;
     await renderPokemon(value);
+  };
+
+  const handlePrevNext = async (buttonType: string) => {
+    if (buttonType === "next") {
+      setPokemonSearch((prev) => prev + 1);
+      renderPokemon(pokemonSearch);
+    } else {
+      if (pokemonSearch > 1) {
+        setPokemonSearch((prev) => prev - 1);
+        renderPokemon(pokemonSearch);
+      }
+    }
   };
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -54,10 +77,14 @@ const Pokedex = () => {
   return (
     <PokedexContainer>
       {pokemonImage && <PokemonImage src={pokemonImage} alt="pokemon" />}
-      {data && (
+      {data ? (
         <h1>
-          <span className="poke_number">{data.id}</span>-
+          <span className="poke_number">{data.id}</span> -{" "}
           <span className="poke_name">{data.name}</span>
+        </h1>
+      ) : (
+        <h1>
+          <span>{message}</span>
         </h1>
       )}
       <PokemonSearch onSubmit={handleSubmit}>
@@ -70,8 +97,8 @@ const Pokedex = () => {
         />
       </PokemonSearch>
       <PokemonButtons>
-        <button>&lt; Prev</button>
-        <button>Next &gt;</button>
+        <button onClick={() => handlePrevNext("prev")}>&lt; Prev</button>
+        <button onClick={() => handlePrevNext("next")}>Next &gt;</button>
       </PokemonButtons>
       <PokedexImage src={pokedexImage} alt="pokedex" />
     </PokedexContainer>
